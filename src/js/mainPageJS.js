83d4c8162612a504cdb667787cc4796f17d23632
petaa1 <petaale03@gmail.com>
2023-04-17 21:27:00 +0800

    Changes to Input Message Box

--- src/js/mainPageJS.js
+++ src/js/mainPageJS.js
@@ -10,20 +10,61 @@ document.getElementById("main").style.marginLeft= "0";
 document.body.style.backgroundColor = "white";
 }
 
-function insertText() {
-    let theText = document.getElementById("insertText").value;
-    if(theText != ''){
-        document.getElementById("insertText").value = "";
-        let theTextSet = document.createElement("p");
-        theTextSet.innerText="Tony: "+theText;
-        let textMessage = document.getElementById("textMessage");
-        textMessage.appendChild(theTextSet);
-        textMessage.scrollTop =  textMessage.scrollHeight - textMessage.clientHeight;
-    }
-}
 
-function enterInsert(){
-    if(event.key === 'Enter') {
-        insertText();        
-    }
+//___________________________________________________________
+// Message Box
+const textarea = document.querySelector("#insertText");
+textarea.addEventListener("keyup", e =>{
+  textarea.style.height = "0px";
+  let scHeight = e.target.scrollHeight;
+  textarea.style.height = `${scHeight}px`;
+});
+
+const form = document.querySelector("#input")
+const output = document.querySelector("#textMessage")
+
+
+form.addEventListener("submit", e =>{
+  e.preventDefault();
+  insertText()
+});
+
+textarea.addEventListener("keydown", e =>{
+  if(e.keyCode == 13 && !e.shiftKey){
+    e.preventDefault();
+    insertText()
+  }
+})
+
+function insertText() {
+  const formData = new FormData(form); // create new FormData object from form data
+  const myInputValue = formData.get('myInput'); // get value of myInput field
+  if(myInputValue != ''){
+    let theTextSet = document.createElement("p");
+    theTextSet.innerText="Tony: "+myInputValue;
+    output.appendChild(theTextSet);
+    output.scrollTop =  output.scrollHeight - output.clientHeight;
+  }
+
+  textarea.value = ''; // clear the textarea value
+
 }
+//___________________________________________________________
+
+// function insertText() {
+//     let theText = document.getElementById("insertText").value;
+//     if(theText != ''){
+//         document.getElementById("insertText").value = "";
+//         let theTextSet = document.createElement("p");
+//         theTextSet.innerText="Tony: "+theText;
+//         let textMessage = document.getElementById("textMessage");
+//         textMessage.appendChild(theTextSet);
+//         textMessage.scrollTop =  textMessage.scrollHeight - textMessage.clientHeight;
+//     }
+// }
+
+// function enterInsert(){
+//     if(event.key === 'Enter') {
+//         insertText();        
+//     }
+// }
